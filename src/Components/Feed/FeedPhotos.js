7c7c60e802471c56d84e7ac1c8044c848ef4c8e2
Cesar <cesar_ubee@hotmail.com>
2022-02-15 16:52:50 -0300

    feed modal

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -3,20 +3,20 @@ import FeedPhotosItem from "./FeedPhotosItem";
 import useFetch from "../../Hooks/useFetch";
 import { PHOTOS_GET } from "../../api";
 import Error from "../Helper/Error";
-import Loading from "./Loading";
+import Loading from "../Helper/Loading";
 import styles from "./FeedPhotos.module.css";
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const { response, json } = await request(url, options);
-      console.log(json);
+      console.log("esse json ?", json, response, url, options);
     }
     fetchPhotos();
-  }, []);
+  }, [request]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
@@ -24,7 +24,11 @@ const FeedPhotos = () => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotosItem key={photo.id} photo={photo} />
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </ul>
     );
---
+++ src/Components/Photo/PhotoComments.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PhotoComments = ({ photo }) => {
+  return <div></div>;
+};
+
+export default PhotoComments;
