5ced4936936b9d2292d57c912ee74ac1e475891d
Cesar <cesar_ubee@hotmail.com>
2022-02-18 09:49:38 -0300

    foto pagina

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -5,23 +5,30 @@ import FeedPhotos from "./FeedPhotos";
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
   const [pages, setPages] = React.useState([1]);
+  const [infinite, setInfinite] = React.useState(true);
 
   React.useEffect(() => {
-    function infiniteScroll(event) {
-      const scroll = window.scrollY;
-      const height = document.body.offsetHeight - window.innerHeight;
-      console.log(scroll);
-      console.log(height);
-      // setPages((pages) => [...pages, pages.length + 1]);
-      console.log(event);
+    let wait = false;
+    function infiniteScroll() {
+      if (infinite) {
+        const scroll = window.scrollY;
+        const height = document.body.offsetHeight - window.innerHeight;
+        if (scroll > height * 0.75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, 500);
+        }
+      }
     }
     window.addEventListener("wheel", infiniteScroll);
     window.addEventListener("scroll", infiniteScroll);
     return () => {
-      window.addEventListener("wheel", infiniteScroll);
-      window.addEventListener("scroll", infiniteScroll);
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
     };
-  }, []);
+  }, [infinite]);
 
   return (
     <div>
@@ -34,6 +41,7 @@ const Feed = ({ user }) => {
           user={user}
           page={page}
           setModalPhoto={setModalPhoto}
+          setInfinite={setInfinite}
         />
       ))}
     </div>
